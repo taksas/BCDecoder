978d28d29a321cf8ef56bdba2fe2333b465648e0
taksas <takuminakayama4869@gmail.com>
2024-01-07 19:18:15 +0900

    [cli] frame_l2

--- BCD_Cli.py
+++ BCD_Cli.py
@@ -1,5 +1,6 @@
 import customtkinter
 import keyboard
+from PIL import Image
 
 
 # --- Global Variables ---
@@ -11,12 +12,54 @@ show_help = True
 
 
 # -------- Texts ---------
-text_raw_image = "(これはヘルプ画面です。F1キー押下で表示切替)\n入力されたバーコード画像"
-text_raw_compare = "機械学習版デコーダーと\nPythonライブラリのデコーダーの結果を比較"
-text_raw_merchandise_search = "特定したJANコードから商品検索。要ネット接続"
-text_raw_log = "各種設定"
-text_raw_sanity_check = "構成\nチェック"
+text_raw_image = "入力されたバーコード画像を表示"
+text_raw_compare = "機械学習版デコーダー\nと\nPythonライブラリのデコーダー  の結果を比較"
+text_raw_merchandise_search = "特定したJANコードから商品検索(要ネット接続)"
+text_raw_log = "操作エリア"
+text_raw_sanity_check = "F1キーで\n画面切替"
+frame_l2__frame_upper_2__desc = "↑ライブラリ版      VS      機械学習版↓"
 # ------------------------
+
+
+
+
+def resize_image_with_aspect_ratio(image_path, target_size):
+    # 画像の読み込み
+    img = Image.open(image_path)
+
+    # 元のサイズ取得
+    original_width, original_height = img.size
+
+    # ターゲットサイズとのアスペクト比比較
+    target_width, target_height = target_size
+    aspect_ratio_target = target_width / target_height
+    aspect_ratio_original = original_width / original_height
+
+    # リサイズ後のサイズ計算
+    if aspect_ratio_original > aspect_ratio_target:
+        new_width = target_width
+        new_height = int(target_width / aspect_ratio_original)
+    else:
+        new_height = target_height
+        new_width = int(target_height * aspect_ratio_original)
+
+    # 画像のリサイズ
+    img = img.resize((new_width, new_height), Image.LANCZOS)
+
+    # 黒色の背景画像作成
+    background = Image.new('RGB', target_size, (0, 0, 0))
+    
+    # 背景に画像を中央に貼り付け
+    x = (target_width - new_width) // 2
+    y = (target_height - new_height) // 2
+    background.paste(img, (x, y, x + new_width, y + new_height))
+
+    # 保存
+    return background
+
+
+
+
 
 
 
@@ -69,7 +112,8 @@ class App(customtkinter.CTk):  # CustomTKinter (GUI) Class
         self.help_txt_sanity_check.place(x=1, y=1)
 
         self.pack()
-        return self # 返す☆彡
+        return self # 返す
+        
 
     
     # ベースフレーム
@@ -77,10 +121,12 @@ class App(customtkinter.CTk):  # CustomTKinter (GUI) Class
         self.frame_l1 = customtkinter.CTkFrame(master=self, border_color="gray", border_width=1, width=1500, height=470)
         self.frame_l1.grid(row=0, column=0, padx=15, pady=15, sticky="nsew")
         self.frame_l1.place(x=10, y=10)
+        self.create_each_components_l1(self.frame_l1)
 
         self.frame_l2 = customtkinter.CTkFrame(master=self, border_color="gray", border_width=1, width=1500, height=350)
         self.frame_l2.grid(row=0, column=0, padx=15, pady=15, sticky="nsew")
         self.frame_l2.place(x=10, y=490)
+        self.create_each_components_l2(self.frame_l2)
 
         self.frame_l3 = customtkinter.CTkFrame(master=self, border_color="gray", border_width=1, width=1500, height=140)
         self.frame_l3.grid(row=0, column=0, padx=15, pady=15, sticky="nsew")
@@ -95,6 +141,48 @@ class App(customtkinter.CTk):  # CustomTKinter (GUI) Class
         self.frame_r2.grid(row=0, column=0, padx=15, pady=15, sticky="nsew")
         self.frame_r2.place(x=1520, y=790)
         
+    
+
+    # L1コンポーネントを作成（画像を表示するやつ）
+    def create_each_components_l1(self, self_l1):
+        img = resize_image_with_aspect_ratio("Resources/icon.png", (1490, 470))
+        tk_image = customtkinter.CTkImage(light_image=img, size=(1490, 470))
+        self_l1.image_area = customtkinter.CTkLabel(master=self_l1, image=tk_image, text='')
+        self_l1.image_area.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+    
+
+    # L2コンポーネントを作成（pythonライブラリと機械学習版を比較するやつ）
+    def create_each_components_l2(self, self_l2):
+
+        # ライブラリ版部分
+        self_l2.frame_upper_1 = customtkinter.CTkFrame(master=self_l2, fg_color="transparent", border_width=0, width=1480, height=100)
+        self_l2.frame_upper_1.place(x=10, y=10)
+        self_l2.frame_upper_1.squares = []
+        for i in range(13):
+            temp_square = customtkinter.CTkFrame(master=self_l2.frame_upper_1, border_color="gray", border_width=1, width=85, height=85)
+            temp_square.grid(row=0, column=i, padx=5, pady=5, sticky="s")
+            self_l2.frame_upper_1.squares.append(temp_square)
+        customtkinter.CTkLabel(master=self_l2.frame_upper_1, text="? ? ? s", font=default_font_65px).grid(row=0, column=13, padx=5, pady=5, sticky="s") # 処理速度計測用
+
+        # 説明部分
+        self_l2.frame_upper_2 = customtkinter.CTkFrame(master=self_l2, fg_color="transparent", border_width=0, width=1480, height=100)
+        self_l2.frame_upper_2.place(x=10, y=120)
+        customtkinter.CTkLabel(master=self_l2.frame_upper_2, text=frame_l2__frame_upper_2__desc, font=default_font_65px).place(x=10, y=10)
+
+        # 機械学習版部分
+        self_l2.frame_upper_3 = customtkinter.CTkFrame(master=self_l2, fg_color="transparent", border_width=0, width=1480, height=100)
+        self_l2.frame_upper_3.place(x=10, y=240)
+        self_l2.frame_upper_3.squares = []
+        for i in range(13):
+            temp_square = customtkinter.CTkFrame(master=self_l2.frame_upper_3, border_color="gray", border_width=1, width=85, height=85)
+            temp_square.grid(row=0, column=i, padx=5, pady=5, sticky="s")
+            self_l2.frame_upper_3.squares.append(temp_square)
+            # print(temp_square)
+        customtkinter.CTkLabel(master=self_l2.frame_upper_3, text="? ? ? s", font=default_font_65px).grid(row=0, column=13, padx=5, pady=5, sticky="s") # 処理速度計測用
+
+
+        return
+
 
 
 
