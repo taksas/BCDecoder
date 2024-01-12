9ad25028cc93aad867a5236c87e0d2af985f8ddb
taksas <takuminakayama4869@gmail.com>
2024-01-12 14:00:37 +0900

    Add Log

---
+++ Researches/Logs/20240112_1.py
@@ -0,0 +1,208 @@
+# v9_240111
+# (1000万個のデータセット導入検討時、番目指定印加処理が重くなりすぎ半日かかる予想になったので、その処理をBCD_Data_Processing_for_Input.pyとして別個に分けた。これで、再起動の度に印加処理をしなくて良くなった)
+
+#%%
+# 必要に応じてpip
+# !pip install --upgrade pip
+# !pip install numpy scikit-learn tensorflow matplotlib pillow pandas
+# !pip install seaborn
+
+
+
+#%%
+# 機械学習のライブラリ関連をインポート
+import pandas as pd
+import numpy as np
+
+# ランダムにシャッフルして，学習・テストに分割するモジュール
+from sklearn.model_selection import ShuffleSplit
+
+# 評価指標の計算用
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+
+# 深層学習のライブラリをインポート
+import tensorflow as tf
+import tensorflow.keras as keras
+
+#表示系のインポートと設定
+import matplotlib.pyplot as plt
+
+# ファイル操作
+import os
+
+# 画像操作
+from PIL import Image
+
+# 時間取得
+import datetime
+
+
+
+
+#%%
+# DIRS
+DATASET_NUM = 100000
+DIRS_DATASET = "../Training/Datasets_v0108_AfterProcessing/" + str(DATASET_NUM) + ".npz"
+
+
+
+#%%
+# NpzFile形式からXとyを取得
+npz_kw = np.load(DIRS_DATASET)
+X = npz_kw["arr_0"]
+y = npz_kw["arr_1"]
+
+#%%
+print(type(X))
+X
+#%%
+print(type(y))
+y
+
+#%%
+# ラベルデータをone-hotベクトル「から」直す
+def one_hot_vector_restore(y):
+    labels = {
+        (1, 0, 0, 0, 0, 0, 0, 0, 0, 0): 0,
+        (0, 1, 0, 0, 0, 0, 0, 0, 0, 0): 1,
+        (0, 0, 1, 0, 0, 0, 0, 0, 0, 0): 2,
+        (0, 0, 0, 1, 0, 0, 0, 0, 0, 0): 3,
+        (0, 0, 0, 0, 1, 0, 0, 0, 0, 0): 4,
+        (0, 0, 0, 0, 0, 1, 0, 0, 0, 0): 5,
+        (0, 0, 0, 0, 0, 0, 1, 0, 0, 0): 6,
+        (0, 0, 0, 0, 0, 0, 0, 1, 0, 0): 7,
+        (0, 0, 0, 0, 0, 0, 0, 0, 1, 0): 8,
+        (0, 0, 0, 0, 0, 0, 0, 0, 0, 1): 9,
+    }
+
+    y = np.array([labels[tuple(one_hot)] for one_hot in y])
+    return y
+
+
+
+#%%
+### データの分割
+ss = ShuffleSplit(n_splits=1,      # 分割を1個生成
+                  train_size=0.8,  # 学習
+                  test_size =0.2,  # テスト
+                  random_state=0)  # 乱数種（再現用）
+
+# 学習データとテストデータのインデックスを作成
+train_index, test_index = next(ss.split(X))
+
+X_train, X_test = X[train_index], X[test_index] # 学習データ，テストデータ
+y_train, y_test = y[train_index], y[test_index] # 学習データのラベル，テストデータのラベル
+
+
+
+
+#%%
+X
+#%%
+y
+#%%
+X_train.shape[0], X_train.shape[1]
+#%%
+X_train
+#%%
+### ニューラルネットワークの構築
+
+# 学習し、テストデータで評価し、スコアを表示する
+# 引数は、中間層の数、バッチサイズ、epoch数
+
+def fit_epoch(neuron, batch, epochs, ckpt_period, optimizer_name):
+    ver_name = "v9_240111"
+    
+    # チェックポイントの設定
+    dt_now = datetime.datetime.now()
+    checkpoint_path = "./training_ckpt_" + dt_now.strftime('%Y%m%d%H%M%S') + "_" + ver_name + "_d" + str(DATASET_NUM) + "_n" + str(neuron)  + "_b" + str(batch) + "_e" + str(epochs) + "_c" + str(ckpt_period) + "_" + optimizer_name + "/cp-{epoch:09d}.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(
+        checkpoint_path,
+        verbose=1,
+        save_weights_only=True,
+        period=ckpt_period  # 重みをckpt_periodエポックごとに保存します
+    )
+
+
+    # レイヤーのオブジェクトを作成
+    Dense = keras.layers.Dense
+
+    # モデルの構造を定義
+    model = keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten(input_shape=(337, )))
+    model.add(Dense(neuron, activation='relu'))
+    
+    # 畳み込み層を追加
+    # model.add(tf.keras.layers.Flatten(tf.keras.layers.Conv2D(filters=neuron, kernel_size=(3, 3), activation='relu', input_shape=(1, 337, 3))))
+
+    model.add(Dense(10, activation='softmax')) # 10つのラベルがありsoftmaxで最後の層作る
+
+    # モデルを構築
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=optimizer_name,
+        metrics=['accuracy']
+    )
+
+    # 必要に応じてチェックポイントから再開
+    model.load_weights("./training_ckpt_20240112021047_v9_240111_d100000_n1024_b32_e8000_c100_Adamax/cp-000000100.ckpt")
+
+    # 学習を実行
+    hist = model.fit(X_train, y_train,
+        batch_size=batch, # 誤差逆伝播法をするときの1回当たりのデータ数
+        epochs=epochs,
+        callbacks=[cp_callback],
+        verbose=1,
+        validation_split=0.1)
+    
+    # モデルの保存
+    model.save("./TrainedModel/" + dt_now.strftime('%Y%m%d%H%M%S') + "_" + ver_name + "_d" + str(DATASET_NUM) + "_n" + str(neuron)  + "_b" + str(batch) + "_e" + str(epochs) + "_"+ optimizer_name)
+    
+    # モデルを評価
+    score = model.evaluate(X_test, y_test, verbose=1)
+    print('正解率(Accuracy)=', score[1], 'loss=', score[0])
+
+    # 予測を取得
+    predictions = model.predict(X_test)
+    predicted_labels = tf.argmax(predictions, axis=1).numpy()
+    y_test_restored = one_hot_vector_restore(y_test) # one-hotベクトル「から」直す
+
+    print(predicted_labels)
+
+    # classification_reportを使用して評価指標を表示
+    df_report = pd.DataFrame(classification_report(y_test_restored, predicted_labels, output_dict=True)).T
+    print(df_report)
+
+    # seabornのヒートマップ
+    sns.heatmap(confusion_matrix(y_test_restored, predicted_labels), annot=True)
+    plt.xlabel("pred")
+    plt.ylabel('true')
+    plt.show()
+    
+    # 学習の様子をグラフへ描画 
+    # 正解率の推移をプロット
+    plt.plot(hist.history['accuracy'])
+    plt.plot(hist.history['val_accuracy'])
+    plt.title('Accuracy')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    # ロスの推移をプロット
+    plt.plot(hist.history['loss'])
+    plt.plot(hist.history['val_loss'])
+    plt.title('Loss')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()   
+
+
+
+
+#%%
+print(DATASET_NUM)
+#%%
+# fit_epoch(中間層の数, バッチサイズ, 学習回数, チェックポイントの作成タイミング, 最適化関数)
+fit_epoch(     1024,          32,        8000,                  100,              "Adamax")
+
+# %%
